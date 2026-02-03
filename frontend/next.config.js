26e8cc39a726621a79267649ecaa644205721bf2
agyn-ub <agyn.bolatov.1992@gmail.com>
2026-02-03 20:39:09 +0500

    turbopack

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding', '@react-native-async-storage/async-storage');
-    return config;
-  },
 };
 
 module.exports = nextConfig;
